1a53325b31a888874dc615ca0869c21162f2d689
sande <sande@SANDESH>
2024-06-25 20:40:19 +0530

    Second_commit

--- corejava/src/com/tnsif/encapsulation/Encapsulation.java
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -1,11 +1,30 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum=10,age;
-	String name;
-	
-	void show()
-	{
-		System.out.println("serialnumber: "+serialNum+"\t Name: "+name +"\t Age:"+age);
+	private int serialNum=10,age;
+	private String name;
+	public int getSerialNum() {
+		return serialNum;
 	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", age=" + age + ", name=" + name + "]";
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
--- corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -5,10 +5,10 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Encapsulation ob=new Encapsulation();
-		ob.age=789;
-		ob.serialNum=55;
-		ob.name="Sandesh";
-		ob.show();
+		ob.setAge(21);
+		ob.setName("Sandesh");
+		ob.setSerialNum(789);
+		System.out.println(ob);
 
 	}
 

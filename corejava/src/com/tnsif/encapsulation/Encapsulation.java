ad19cc6f3bb7df32e15886672502029accd3a9b5
sande <sande@SANDESH>
2024-06-25 18:52:13 +0530

    FirstCommit

---
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,11 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10,age;
+	String name;
+	
+	void show()
+	{
+		System.out.println("serialnumber: "+serialNum+"\t Name: "+name +"\t Age:"+age);
+	}
+}
---
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();
+		ob.age=789;
+		ob.serialNum=55;
+		ob.name="Sandesh";
+		ob.show();
+
+	}
+
+}

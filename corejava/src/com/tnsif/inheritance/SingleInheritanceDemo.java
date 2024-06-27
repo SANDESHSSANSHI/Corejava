9e96fe6b49c4363abc0174927faf46476802c75a
sande <sande@SANDESH>
2024-06-27 14:17:11 +0530

    Single Inheritance Program

---
+++ corejava/src/com/tnsif/inheritance/SingleInheritanceDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.inheritance;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ob = new Student("Sandesh",81,636368001,45789889,"dbit",5753);
+		System.out.println(ob);
+	}
+
+}

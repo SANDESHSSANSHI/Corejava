6de5be662658b29fb9411ab4f4b22887340dd14e
sande <sande@SANDESH>
2024-06-30 08:18:28 +0530

    Revert "Revert "Multiple_Inheritance""
    This reverts commit 0a4d37999c6457d4450a3c9d628ce88802ac73f9.

---
+++ corejava/src/com/sandesh/assignment03/MultipleInheritance.java
@@ -0,0 +1,12 @@
+package com.sandesh.assignment03;
+
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		State st=new State("Nile","Mtpiku","mon","liberty","ganga","kanchajunga","tajmahal","Kaveri","himadGopalswamy");
+		System.out.println(st);
+
+	}
+
+}

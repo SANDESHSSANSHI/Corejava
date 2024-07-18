8e386da34dff05f46a5b0e911fbeeef289c8b778
sande <sande@SANDESH>
2024-07-19 04:54:15 +0530

    Static Programs

---
+++ corejava/src/com/tnsif/staticprograms/Demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.staticprograms;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The college name is:"+ Student.collegename); // access static variables
+		Student ob=new Student("Sandesh S Sanshi",81);
+		System.out.println(ob);
+	}
+
+}

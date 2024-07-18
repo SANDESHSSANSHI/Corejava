b3e4942078fb12be3cbce6fc8152b645ca087d37
sande <sande@SANDESH>
2024-07-19 04:45:00 +0530

    Interface Program

---
+++ corejava/src/com/tnsif/interfacePrograms/Demo.java
@@ -0,0 +1,15 @@
+package com.tnsif.interfacePrograms;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Amazon.aboutUs();
+		Amazon ob=new NonPrime();
+		ob.welcome();
+		ob.servicecharge();
+		ob.thankyou();
+
+	}
+
+}
